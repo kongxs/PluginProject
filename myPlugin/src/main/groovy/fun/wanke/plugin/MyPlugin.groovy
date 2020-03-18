f7c421691abf34defdb9289e25d0fe54e19dd3f7
kongxiangshu <kongxiangshu@jd.com>
2020-03-18 20:35:52 +0800

    增加参数

--- myPlugin/src/main/groovy/fun/wanke/plugin/MyPlugin.groovy
+++ myPlugin/src/main/groovy/fun/wanke/plugin/MyPlugin.groovy
@@ -3,6 +3,7 @@ package fun.wanke.plugin
 import com.android.build.gradle.AppExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
 
 class MyPlugin implements Plugin<Project> {
     void apply(Project project) {
@@ -13,6 +14,15 @@ class MyPlugin implements Plugin<Project> {
 
         project.extensions.create("extParams",ExtParams)
 
+        project.configurations.all { configuration ->
+            def name = configuration.name
+            System.out.println("this configuration is ${name}")
+            if (name != "implementation" && name != "compile") {
+                return
+            }
+            //为Project加入Gson依赖
+            configuration.dependencies.add(project.dependencies.create("com.google.code.gson:gson:2.8.2"))
+        }
 
         ExtParams params = project.extParams
 
@@ -21,6 +31,26 @@ class MyPlugin implements Plugin<Project> {
 
         def android = project.extensions.getByType(AppExtension)
         android.registerTransform(new CodeTrans(project))
+
+        //将jdJar task 挂到构建生命周期中
+
+        def checkTask = project.tasks.getByName("check")
+        def buildTask = project.tasks.getByName("build")
+        def jdJarTask = project.tasks.getByName("jdJar")
+
+        jdJarTask.dependsOn(checkTask)
+        buildTask.dependsOn(jdJarTask)
+
+//        jdJarTask.mustRunAfter buildTask
+
+
+        buildTask.doLast {
+            println("build task finished ....... ")
+
+
+        }
+
+
 
 
     }
